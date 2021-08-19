335842382f779e9f63fce936e91b0bb661b3cfc5
Todd Granados <Toddgranados@gmail.com>
2021-08-18 22:53:49 -0400

    Add files via upload

---
+++ Assets/js/questions.js
@@ -0,0 +1,33 @@
+var questions = [
+  {
+    title: " Where does the show take place ?",
+    choice: ["Manhattan", "Brooklyn", "Chicago", "Long Island"],
+    answer: "Manhattan",
+  },
+  {
+    title: " What year is Ted narrating the story from ?",
+    choice: ["2020", "2027", "2030", "2036"],
+    answer: "2030",
+  },
+  {
+    title: " What instrument does Ted steal for Robin ?",
+    choice: ["A trumpet", "A Tuba", "A saxophone", "A french horn"],
+    answer: "A french horn",
+  },
+  {
+    title: "Who does Barney believe is his father? ",
+    choice: ["Alex Trebek", "Dick Clark", "Peter Marshall", "Bob Barker"],
+    answer: "Bob Barker",
+  },
+  {
+    title: " What embarrassing tattoo does Ted get in Season 3?",
+    choice: ["A unicorn", "a Robin", "A butterfly", "A kitty cat"],
+    answer: "A butterfly",
+  },
+  {
+    title:
+      " In Season 4, Episode 22, Barney says he has slept with how many women?",
+    choice: ["200", "100", "87", "69"],
+    answer: "200",
+  },
+];
